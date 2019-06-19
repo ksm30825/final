4622a0de740909d40209307b70a7a2eb728400f1
User <User@DESKTOP-B1UCMP5>
2019-06-19 20:01:17 +0900

    채팅방 - 클래스 설계 하는중..ㅜㅜ

---
+++ src/main/java/com/kh/ti/Chatting/model/dao/ChattingDao.java
@@ -0,0 +1,8 @@
+package com.kh.ti.Chatting.model.dao;
+
+public interface ChattingDao {
+
+	//지원 - 채팅방 만들기
+	public int insertChatting();
+	
+}
---
+++ src/main/java/com/kh/ti/Chatting/model/service/ChattingService.java
@@ -0,0 +1,7 @@
+package com.kh.ti.Chatting.model.service;
+
+public interface ChattingService {
+
+	//채팅방 만들기 - 지원
+	public int insertChatting();
+}
