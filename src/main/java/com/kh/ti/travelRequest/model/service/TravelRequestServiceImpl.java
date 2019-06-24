b9f1b5d70e72a0d62dae09cbfa99cb5ff739e0c4
USER <USER@DESKTOP-DVQO56D>
2019-06-24 20:34:56 +0900

    여행 의뢰하기 기능 구현/pom,root-xml AOP트랜잭션처리 설정

--- src/main/java/com/kh/ti/travelRequest/model/service/TravelRequestServiceImpl.java
+++ src/main/java/com/kh/ti/travelRequest/model/service/TravelRequestServiceImpl.java
@@ -1,5 +1,27 @@
 package com.kh.ti.travelRequest.model.service;
 
-public class TravelRequestServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Service;
+
+import com.kh.ti.travelRequest.model.dao.TravelRequestDao;
+import com.kh.ti.travelRequest.model.vo.TravelRequest;
+
+@Service
+public class TravelRequestServiceImpl implements TravelRequestService{
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	@Autowired
+	private TravelRequestDao trd;
+	@Autowired
+	private DataSourceTransactionManager transactionManager;
+	
+	//의뢰하기 신청
+	@Override
+	public int insertRequest(TravelRequest tr) {
+		return trd.insertRequest(sqlSession, tr);
+	}
 
 }
