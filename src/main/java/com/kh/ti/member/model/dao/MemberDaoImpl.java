9061fd7baaf8cf7ad0a16aed0854412f1c8c1d0a
psr <psr@DESKTOP-T1ATB98>
2019-06-25 20:28:12 +0900

    자사회원가입 구현 완료(휴대폰인증구현안함), 로그인 구현 완료, 로그아웃 구현 완료, 네비바수정, porm설정추가

--- src/main/java/com/kh/ti/member/model/dao/MemberDaoImpl.java
+++ src/main/java/com/kh/ti/member/model/dao/MemberDaoImpl.java
@@ -8,11 +8,16 @@ import com.kh.ti.member.model.vo.Member;
 @Repository
 public class MemberDaoImpl implements MemberDao{
 
+	//비밀번호 확인용메소드 - 세령
+	@Override
+	public String selectEncPassword(SqlSessionTemplate sqlSession, Member m) {
+		return sqlSession.selectOne("Member.selectPwd", m.getEmail());
+	}
+	
 	//로그인용메소드 - 세령
 	@Override
-	public Member loginCheck(SqlSessionTemplate sqlSession, Member m) {
-		Member loginUser = sqlSession.selectOne("Member.loginCheck", m);
-		return loginUser;
+	public Member selectMember(SqlSessionTemplate sqlSession, Member m) {
+		return sqlSession.selectOne("Member.selectLoginUser", m);
 	}
 
 	//회원가입용메소드 - 세령
@@ -20,5 +25,12 @@ public class MemberDaoImpl implements MemberDao{
 	public int insertMember(SqlSessionTemplate sqlSession, Member m) {
 		return sqlSession.insert("Member.insertMember", m);
 	}
+
+	//가입 포인트 적립용 메소드 - 세령
+	@Override
+	public int insertReservePoint(SqlSessionTemplate sqlSession, String email) {
+		return sqlSession.insert("Member.insertReservePoint", email);
+	}
+
 
 }
--- src/main/java/com/kh/ti/member/model/service/MemberService.java
+++ src/main/java/com/kh/ti/member/model/service/MemberService.java
@@ -1,10 +1,11 @@
 package com.kh.ti.member.model.service;
 
+import com.kh.ti.member.model.exception.LoginException;
 import com.kh.ti.member.model.vo.Member;
 
 public interface MemberService {
 
-	Member loginMember(Member m);
+	Member loginMember(Member m) throws LoginException;
 
 	int insertMember(Member m);
 	
